c8c8b37dbd32a0fed2ee50eb96a3f82dc36988de
Parv-gugnani <9765.parv@gmail.com>
2024-07-01 12:37:45 +0530

    drizzle error working

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,6 @@
-import { PgTable, text } from "drizzle-orm/pg-core";
+import { pgTable, text } from "drizzle-orm/pg-core";
 
-export const accounts = PgTable("accounts", {
+export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
   name: text("name").notNull(),
   userId: text("user_id").notNull(),
