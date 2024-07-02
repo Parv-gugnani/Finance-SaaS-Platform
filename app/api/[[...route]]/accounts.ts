472c5331ae074c0b0d8193988c75e30857d28246
Parv-gugnani <9765.parv@gmail.com>
2024-07-02 12:48:32 +0530

    get account

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,8 +1,16 @@
 import { accounts } from "@/db/schema";
 import { Hono } from "hono";
+import { db } from "@/db/drizzle";
 
-const app = new Hono().get("/", (c) => {
-  return c.json({ accounts: [] });
+const app = new Hono().get("/", async (c) => {
+  const data = await db
+    .select({
+      id: accounts.id,
+      name: accounts.name,
+    })
+    .from(accounts);
+
+  return c.json({ data });
 });
 
 export default app;
---
+++ providers/query-provider.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function QueryProviders({ children }: Props) {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
