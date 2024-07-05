da00f02e040919b5b29ecdcf35974473e4912397
Parv-gugnani <9765.parv@gmail.com>
2024-07-05 17:53:52 +0530

    account apig

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,14 +1,24 @@
 import { accounts } from "@/db/schema";
 import { Hono } from "hono";
 import { db } from "@/db/drizzle";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { HTTPException } from "hono/http-exception";
+import { eq } from "drizzle-orm";
 
-const app = new Hono().get("/", async (c) => {
+const app = new Hono().get("/", clerkMiddleware(), async (c) => {
+  const auth = getAuth(c);
+
+  if (!auth?.userId) {
+    return c.json({ error: "Unauthorized" }, 401);
+  }
+
   const data = await db
     .select({
       id: accounts.id,
       name: accounts.name,
     })
-    .from(accounts);
+    .from(accounts)
+    .where(eq(accounts.userId, auth.userId));
 
   return c.json({ data });
 });
