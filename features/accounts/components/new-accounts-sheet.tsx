572afb2dd5d5106c64b3080b30c3b53f578b692f
Parv-gugnani <9765.parv@gmail.com>
2024-07-22 22:11:18 +0530

    testing values

--- features/accounts/components/new-accounts-sheet.tsx
+++ features/accounts/components/new-accounts-sheet.tsx
@@ -6,10 +6,20 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import { useNewAccount } from "../hooks/use-new-accounts";
+import { AccountForm } from "./account-form";
+import { insertAccountSchema } from "@/db/schema";
+import { z } from "zod";
+
+const formSchema = insertAccountSchema.pick({ name: true });
+
+type FormValues = z.input<typeof formSchema>;
 
 export const NewAccountSheet = () => {
   const { isOpen, onClose } = useNewAccount();
 
+  const onSubmit = (values: FormValues) => {
+    console.log({ values });
+  };
   //
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
@@ -20,6 +30,7 @@ export const NewAccountSheet = () => {
             Create a new Account to track your Transactions
           </SheetDescription>
         </SheetHeader>
+        <AccountForm onSubmit={onSubmit} disabled={false} />
       </SheetContent>
     </Sheet>
   );
