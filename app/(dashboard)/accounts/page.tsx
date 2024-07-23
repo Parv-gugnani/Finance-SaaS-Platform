7d7bc3ec056635d5275a26a8706c5390ab7bd58f
Parv-gugnani <9765.parv@gmail.com>
2024-07-23 18:09:18 +0530

    account data table

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,8 +1,20 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts";
 import { Plus } from "lucide-react";
+import { columns, Payment } from "./columns";
+import { DataTable } from "@/components/data-table";
+
+const data: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  // ...
+];
 
 const AccountsPage = () => {
   const newAccount = useNewAccount();
@@ -16,6 +28,9 @@ const AccountsPage = () => {
             Add new
           </Button>
         </CardHeader>
+        <CardContent>
+          <DataTable columns={columns} data={data} />
+        </CardContent>
       </Card>
     </div>
   );
