f0ff0a86a06ea90f65ae22481fb9e6f028d8b8dd
Parv-gugnani <9765.parv@gmail.com>
2024-07-24 16:48:44 +0530

    sorting data

--- app/(dashboard)/accounts/columns.ts
+++ app/(dashboard)/accounts/columns.ts
@@ -1,9 +1,8 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
 export type Payment = {
   id: string;
   amount: number;
