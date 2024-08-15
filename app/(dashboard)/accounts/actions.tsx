17d5eccd0b28f438e29f280636534b2642009c91
Parv-gugnani <9765.parv@gmail.com>
2024-08-15 19:33:20 +0530

    dropdown

--- app/(dashboard)/accounts/actions.tsx
+++ app/(dashboard)/accounts/actions.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Edit, MoreHorizontal } from "lucide-react";
 
 type props = {
   id: string;
@@ -14,7 +16,17 @@ export const Actions = ({ id }: props) => {
   return (
     <>
       <DropdownMenu>
-        <DropdownMenuTrigger asChild></DropdownMenuTrigger>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="size-8 p-0">
+            <MoreHorizontal />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem disabled={false} onClick={() => {}}>
+            <Edit className="size-4 mr-2" />
+            Edit
+          </DropdownMenuItem>
+        </DropdownMenuContent>
       </DropdownMenu>
     </>
   );
