9277b50aa2a81dc98fd6b767c8d6d99bf3abb561
Parv-gugnani <9765.parv@gmail.com>
2024-08-25 17:23:21 +0530

    category  comp

--- app/(dashboard)/categories/page.tsx
+++ app/(dashboard)/categories/page.tsx
@@ -1,12 +1,66 @@
-import { useBulkdDeleteCategories } from "@/features/categories/api/use-bulk-delete-categories";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNewCategory } from "@/features/categories/hooks/use-new-category";
+import { Loader2, Plus } from "lucide-react";
+
+import { column } from "./columns";
+import { DataTable } from "@/components/data-table";
+import { useGetCategories } from "@/features/categories/api/use-get-categories";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useBulkdDeleteCategories } from "@/features/categories/api/use-bulk-delete-categories";
 
 const CategoriesPage = () => {
   const newCategory = useNewCategory();
   const deleteCategories = useBulkdDeleteCategories();
+  const categoriesQuery = useGetCategories();
+  const categories = categoriesQuery.data || [];
+
+  const isDisabled = categoriesQuery.isLoading || deleteCategories.isPending;
+
+  if (categoriesQuery.isLoading) {
+    return (
+      <div className="max-x-screen-2xl mx-auto w-full pb-10 -mt-24">
+        <Card className="border-none drop-shadow-sm">
+          <CardHeader>
+            <Skeleton className="h-8 w-48" />
+          </CardHeader>
+          <CardContent>
+            <div className="h-[500px] w-full flex items-center justify-center">
+              <Loader2 className="size-6 text-slate-300 animate-sping" />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Categories Page</h1>
+    <div className="max-x-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">
+            Categories page
+          </CardTitle>
+          <Button onClick={newCategory.onOpen} size="sm">
+            <Plus className="size-4 mr-2" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            columns={columns}
+            data={categories}
+            filterKey="name"
+            onDelete={(row) => {
+              const ids = row.map((r) => r.original.id);
+              deleteCategories.mutate({ ids });
+            }}
+            disabled={isDisabled}
+          />
+        </CardContent>
+      </Card>
     </div>
   );
 };
--- features/categories/api/use-edit-category.ts
+++ features/categories/api/use-edit-category.ts
@@ -3,32 +3,33 @@ import { InferRequestType, InferResponseType } from "hono";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { client } from "@/lib/hono";
-import { categories } from "@/db/schema";
 
 type ResponseType = InferResponseType<
-  (typeof client.api.categories)["bulk-delete"]["$post"]
+  (typeof client.api.categories)[":id"]["$patch"]
 >;
-
 type RequestType = InferRequestType<
-  (typeof client.api.categories)["bulk-delete"]["$json"]
->;
+  (typeof client.api.categories)[":id"]["$patch"]
+>["json"];
 
-export const useBulkDeleteCategories = () => {
+export const useEditCategory = (id?: string) => {
   const queryClient = useQueryClient();
 
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async (json) => {
-      const response = await client.api.categories["bulk-delete"]["$post"]({
+      const response = await client.api.categories[":id"]["$patch"]({
+        param: { id },
         json,
       });
       return await response.json();
     },
     onSuccess: () => {
-      toast.success("Category deleted");
+      toast.success("Category updated");
+      queryClient.invalidateQueries({ queryKey: ["category", { id }] });
       queryClient.invalidateQueries({ queryKey: ["categories"] });
+      //TODO: Invalidate sumary and transactions
     },
     onError: () => {
-      toast.error("Failed to Delete categories");
+      toast.error("Failed to edit category");
     },
   });
 
