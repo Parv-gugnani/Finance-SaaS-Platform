3dc35de8f58f03496afd7216391d109e02ad6c3e
Parv-gugnani <9765.parv@gmail.com>
2024-08-28 23:12:27 +0530

    transaciton api completed

---
+++ features/transactions/api/use-get-transactions.ts
@@ -0,0 +1,36 @@
+import { client } from "@/lib/hono";
+import { convertAmountFromMiliUnits } from "@/lib/utils";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+
+export const useGetTransactions = () => {
+  const params = useSearchParams();
+  const from = params.get("from") || "";
+  const to = params.get("to") || "";
+  const accountId = params.get("accountId") || "";
+
+  const query = useQuery({
+    // TODO : Check when unique data need or not
+    queryKey: ["transactions"],
+    queryFn: async () => {
+      const response = await client.api.transactions.$get({
+        query: {
+          from,
+          to,
+          accountId,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch transactions");
+      }
+
+      const { data } = await response.json();
+      return data.map((transaction) => ({
+        ...transaction,
+        amount: convertAmountFromMiliUnits(transaction.amount),
+      }));
+    },
+  });
+  return query;
+};
