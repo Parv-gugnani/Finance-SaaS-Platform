fa8109f934c85ed10cd791b7c26cd9856e9abdc8
Parv-gugnani <9765.parv@gmail.com>
2024-09-18 18:31:50 +0530

    drizzle fix

--- drizzle/0000_moaning_mindworm.sql
+++ drizzle/0000_moaning_mindworm.sql
@@ -1,19 +1,17 @@
 CREATE TABLE IF NOT EXISTS "accounts" (
 	"id" text PRIMARY KEY NOT NULL,
-	"plaid_id" text,
 	"name" text NOT NULL,
 	"user_id" text NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "categories" (
 	"id" text PRIMARY KEY NOT NULL,
-	"plaid_id" text,
 	"name" text NOT NULL,
 	"user_id" text NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "transactions" (
-	"id" text,
+	"id" text PRIMARY KEY NOT NULL,
 	"amount" integer NOT NULL,
 	"payee" text NOT NULL,
 	"notes" text,
--- drizzle/0001_low_mesmero.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "transactions" ADD PRIMARY KEY ("id");--> statement-breakpoint
-ALTER TABLE "transactions" ALTER COLUMN "id" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "accounts" DROP COLUMN IF EXISTS "plaid_id";--> statement-breakpoint
-ALTER TABLE "categories" DROP COLUMN IF EXISTS "plaid_id";
